eeef48d513a140f3d3bceea6024bc61c8651aa02
S Sabhari Girish <65026577+girish004@users.noreply.github.com>
2023-01-17 16:03:02 +0530

    Scala exceptions - Futures - Collections

---
+++ akka-http-quickstart-scala/src/main/scala/com/example/Tutorials.scala
@@ -0,0 +1,66 @@
+package com.example
+
+import java.io._
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object Tutorials{
+
+  def exception():Unit={
+    try {
+      val div = 5 / 0
+      val f = new FileReader("input.txt")
+    } catch {
+      case ex: FileNotFoundException => {
+        println("Missing file exception")
+        println(ex)
+      }
+      case ex2: ArithmeticException => {
+        println("Cannot divide by zero")
+        println(ex2)
+      }
+      case ex: IOException => {
+        println("IO Exception")
+      }
+
+    }
+  }
+  def closure_lambda():Unit={
+    var mul= (a:Int,b:Int)=> {
+      var c=a+1
+      c*b*a
+    }
+    println(mul(2,3))
+  }
+  def collections(): Unit={
+    // List
+    var list1=List(1,2,3)
+    list1=list1.updated(1,6)
+    println(list1)
+    // Set
+    var set1=Set(1,2,3,3)
+    println(set1-4-3)
+    // Maps
+    var map1 = Map("Hello"-> "English", "Vanakkam"-> "Telugu")
+    println(map1.keys)
+    println(map1.values)
+    map1+="Namaste"->"Hindi"
+    map1=map1.updated("Vanakkam","Tamil")
+    println(map1)
+
+  }
+  def futures(): Unit = {
+    val fut = Future { Thread.sleep(10000); 21 + 21 }
+    println(fut.isCompleted)
+    Thread.sleep(20000)
+    println(fut.isCompleted)
+  }
+  def main(args: Array[String]): Unit = {
+    exception()
+    closure_lambda()
+    collections()
+    futures()
+
+
+  }
+}
